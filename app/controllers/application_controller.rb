b6f581430ea8fb34a8cbcafdd03bb769d218557f
Christian Bieniak <cbieniak@gmail.com>
2014-07-29 22:53:23 +1000

    Moved token to an ENV variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
 	require 'open-uri'
  def index
- 	channel_data = JSON.load(open("https://slack.com/api/channels.list?token=xoxp-2182263167-2213146441-2494958940-5d527f&pretty=1"))
+ 	channel_data = JSON.load(open("https://slack.com/api/channels.list?token=#{ENV['TOKEN']}&pretty=1"))
 	@channel_data = channel_data["channels"].map{|summary_data| Channel.new(summary_data) }
 	@user_channel_ids = []
 	@channel_identifiers = {}
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 	@user_channel_ids.each do |channel_id|
 		time = Time.now.to_i
 		#https://slack.com/api/channels.history?token=xoxp-2182263167-2213146441-2494958940-5d527f&channel=C025C7R53&oldest=1406507678.000003&count=10&pretty=1
-		message_data = JSON.load(open("https://slack.com/api/channels.history?token=xoxp-2182263167-2213146441-2494958940-5d527f&channel=#{channel_id}&oldest=#{time-3600}&count=10"))
+		message_data = JSON.load(open("https://slack.com/api/channels.history?token=#{ENV['TOKEN']}&channel=#{channel_id}&oldest=#{time-3600}&count=10"))
 		puts message_data
 		@message_data = message_data["messages"].map{|summary_data| Message.new(summary_data) }
 		messages << {@channel_identifiers[channel_id] => @message_data} 
